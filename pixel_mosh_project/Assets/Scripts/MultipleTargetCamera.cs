90b42a7fb4d0a53a29e277201f633ab26deece24
Sahid Enrico De la Rosa Cruz <sahid.enrico@gmail.com>
2020-06-10 22:05:53 -0500

    Camera Script

---
+++ pixel_mosh_project/Assets/Scripts/MultipleTargetCamera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultipleTargetCamera : MonoBehaviour
+{
+    public List<Transform> Targets;
+    public Vector3 Offset;
+    public float SmoothFactor;
+    private Vector3 _Velocity;
+
+    void LateUpdate(){
+        Vector3 center = GetCenterTargets();
+        Vector3 cameraPos = center + Offset;
+        transform.position = Vector3.SmoothDamp(transform.position, cameraPos, ref _Velocity, SmoothFactor);
+    }
+    Vector3 GetCenterTargets(){
+        if(Targets.Count == 1){
+            return Targets[0].position;
+        }
+        var bounds = new Bounds(Targets[0].position, Vector3.zero);
+        for(int i=0; i<Targets.Count; i++){
+            bounds.Encapsulate(Targets[i].position);
+        }
+        return bounds.center;
+    }
+}
