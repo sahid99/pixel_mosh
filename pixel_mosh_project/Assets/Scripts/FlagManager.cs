ac906f8709a684db24fee4554c80f36a34ccccec
Jesus Rojas <rojas964rwb@gmail.com>
2020-06-12 14:10:35 -0500

    CTF Flag Scores

--- pixel_mosh_project/Assets/Scripts/FlagManager.cs
+++ pixel_mosh_project/Assets/Scripts/FlagManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class FlagManager : MonoBehaviour
 {
+    public Vector3 RespawnPoint;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +18,18 @@ public class FlagManager : MonoBehaviour
     }
 
     void OnTriggerEnter2D(Collider2D col){
-        Debug.Log("Choque con" + col.name);
-        this.transform.parent = col.gameObject.transform;
- }
+        if (col.name == "Player1" || col.name == "Player2"){
+            Debug.Log("Choque con" + col.name);
+            this.transform.parent = col.gameObject.transform;
+        }
+        if (col.name == "RedBase"){
+            TDMManager.rScore +=1;
+            this.transform.position = RespawnPoint;
+            this.transform.parent = null;
+        } else if (col.name == "BlueBase"){
+            TDMManager.bScore +=1;
+            this.transform.position = RespawnPoint;
+            this.transform.parent = null;
+        }
+    }
 }
