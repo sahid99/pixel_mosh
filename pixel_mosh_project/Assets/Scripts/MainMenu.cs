9b0e547a892a646bcfa64ecf77f7848231b9fc46
Jesus Rojas <rojas964rwb@gmail.com>
2020-06-12 00:18:40 -0500

    Main Menu Works!

---
+++ pixel_mosh_project/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayTDM(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+    public void PlayFFA(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+2);
+    }
+    public void PlayCTF(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+3);
+    }
+    public void ExitGame(){
+        Application.Quit();   
+    }
+}
